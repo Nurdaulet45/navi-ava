3241ec7d21330912e15fb3cbc0042414b4551871
Tolebi <tolebizaksybaj@gmail.com>
2022-04-07 12:16:42 +0600

    add command artisn

--- routes/dev.php
+++ routes/dev.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::prefix('commands')->group(function () {
     Route::get('optimize-clear', function () {
@@ -15,9 +17,13 @@ Route::prefix('commands')->group(function () {
 });
 
 //Route::group(['prefix' => 'commands', 'middleware' => 'auth'], function () {
-    Route::get('sockets/serve', function () {
-        \Illuminate\Support\Facades\Artisan::call('websockets:serve');
-    });
+Route::get('sockets/serve', function () {
+    try {
+        Artisan::call('websockets:serve');
+    } catch (Exception $e) {
+        Response::make($e->getMessage(), 500);
+    }
+});
 //});
 
 Route::get('/', function () {
