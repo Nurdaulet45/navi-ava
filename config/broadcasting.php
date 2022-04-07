0c10c91f912bb3f37db13a8fdba24d05c0de93f1
Tolebi <tolebizaksybaj@gmail.com>
2022-04-07 12:43:22 +0600

    edit configs

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,7 +40,7 @@ return [
                 'useTLS' => true,
                 'host' => env('APP_URL'),
                 'port' => 6001,
-//                'scheme' => 'http'
+                'scheme' => 'https'
             ],
         ],
 
